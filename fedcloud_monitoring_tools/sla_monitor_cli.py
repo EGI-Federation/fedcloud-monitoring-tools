15bbc3dba115c3da61fe034cddeeff86ec924ff2
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2026-01-15 14:17:08 +0100

    replace appdb with fcis == fed cloud information system

--- fedcloud_monitoring_tools/sla_monitor_cli.py
+++ fedcloud_monitoring_tools/sla_monitor_cli.py
@@ -5,15 +5,15 @@ import importlib
 import click
 import yaml
 from fedcloud_monitoring_tools.accounting import Accounting
-from fedcloud_monitoring_tools.appdb import FedCloudIS
+from fedcloud_monitoring_tools.fedcloud_is import FedCloudIS
 from fedcloud_monitoring_tools.goc import GOCDB
 from fedcloud_monitoring_tools.operations_portal import OpsPortal
 from fedcloudclient.sites import list_sites
 
 
-def check_site_slas(site, acct, appdb, goc, gocdb_sites):
+def check_site_slas(site, acct, fcis, goc, gocdb_sites):
     sla_vos = set()
-    appdb_vos = set(appdb.get_vo_for_site(site))
+    fcis_vos = set(fcis.get_vo_for_site(site))
     click.secho(f"[-] Checking site {site}", fg="blue", bold=True)
     if site not in gocdb_sites:
         click.echo(f"[I] {site} is not present in any SLA")
@@ -28,7 +28,7 @@ def check_site_slas(site, acct, appdb, goc, gocdb_sites):
                 )
             else:
                 click.echo(f"[ERR] {site} has no accounting info for SLA {sla_name}")
-            info_vos = sla["vos"].intersection(appdb_vos)
+            info_vos = sla["vos"].intersection(fcis_vos)
             if info_vos:
                 click.echo(f"[OK] {site} has configured {info_vos} for SLA {sla_name}")
             else:
@@ -45,12 +45,12 @@ def check_site_slas(site, acct, appdb, goc, gocdb_sites):
         )
     if "ops" not in acct.site_vos(site):
         click.echo(f"[W] {site} has no accounting for ops")
-    non_sla_appdb_vos = appdb_vos - sla_vos.union(set(["ops"]))
+    non_sla_fcis_vos = fcis_vos - sla_vos.union(set(["ops"]))
     if non_sla_vos:
         click.echo(
-            f"[W] {site} has VOs {non_sla_appdb_vos} configured but not covered by SLA"
+            f"[W] {site} has VOs {non_sla_fcis_vos} configured but not covered by SLA"
         )
-    if "ops" not in appdb_vos:
+    if "ops" not in fcis_vos:
         click.echo(f"[W] {site} has no configuration for ops")
     click.echo()
 
@@ -63,7 +63,7 @@ def vo_in_map(vo, vo_map):
     return vo in flat_list
 
 
-def check_vo_sla(acct, appdb, goc, ops_portal, user_cert, vo_map, vo):
+def check_vo_sla(acct, fcis, goc, ops_portal, user_cert, vo_map, vo):
     if not vo_in_map(vo, vo_map):
         click.secho(
             "[ERR] VO {} not found in the map file provided".format(vo),
@@ -89,9 +89,9 @@ def check_vo_sla(acct, appdb, goc, ops_portal, user_cert, vo_map, vo):
         return
     sites_gocdb = sorted(all_vos_gocdb[vo])
     sites_acct = sorted([provider for provider in all_vos_acct[vo]])
-    sites_appdb = sorted(appdb.get_sites_for_vo(vo))
+    sites_fcis = sorted(fcis.get_sites_for_vo(vo))
     sites_fedcloudclient = sorted(list_sites(vo))
-    if sites_gocdb == sites_appdb == sites_acct == sites_fedcloudclient:
+    if sites_gocdb == sites_fcis == sites_acct == sites_fedcloudclient:
         click.secho(
             "[OK] VO {}. The sites supporting the VO are: {}".format(vo, sites_gocdb),
             fg="green",
@@ -99,10 +99,10 @@ def check_vo_sla(acct, appdb, goc, ops_portal, user_cert, vo_map, vo):
         )
     elif (
         "sla-group-with-multiple-vos" in sites_gocdb
-        and sites_appdb == sites_acct == sites_fedcloudclient
+        and sites_fcis == sites_acct == sites_fedcloudclient
     ):
         click.secho(
-            "[OK] VO {}. The sites supporting the VO are: {}".format(vo, sites_appdb),
+            "[OK] VO {}. The sites supporting the VO are: {}".format(vo, sites_fcis),
             fg="green",
             bold=True,
         )
@@ -114,7 +114,7 @@ def check_vo_sla(acct, appdb, goc, ops_portal, user_cert, vo_map, vo):
             bold=True,
         )
         click.echo("Sites in GOCDB: {}".format(sites_gocdb))
-        click.echo("Sites in AppDB: {}".format(sites_appdb))
+        click.echo("Sites in AppDB: {}".format(sites_fcis))
         click.echo("Sites in Accounting Portal: {}".format(sites_acct))
         click.echo("Sites in fedcloudclient: {}".format(sites_fedcloudclient))
     click.echo("Accounting data per provider in the last {} days:".format(acct.days))
@@ -144,15 +144,15 @@ def main(
     vo_map = yaml.load(vo_map_src, Loader=yaml.SafeLoader)
     acct = Accounting()
     goc = GOCDB()
-    appdb = FedCloudIS()
+    fcis = FedCloudIS()
     ops_portal = OpsPortal()
 
     if vo:
-        check_vo_sla(acct, appdb, goc, ops_portal, user_cert, vo_map, vo)
+        check_vo_sla(acct, fcis, goc, ops_portal, user_cert, vo_map, vo)
     else:
         gocdb_sites = goc.get_sites_slas(user_cert, vo_map)
         if site:
-            check_site_slas(site, acct, appdb, goc, gocdb_sites)
+            check_site_slas(site, acct, fcis, goc, gocdb_sites)
         else:
             for site in acct.all_sites():
-                check_site_slas(site, acct, appdb, goc, gocdb_sites)
+                check_site_slas(site, acct, fcis, goc, gocdb_sites)
